8f9df7f6053ba908c3b0e973b52d43cb719648ba
chipster6502 <dynamorando@gmail.com>
2026-06-28 12:47:05 +0200

    chore: stop tracking ss_credentials.h (gitignore + CI injection)

--- mister_monitor_CYD28R/ss_credentials.h
+++
@@ -1,7 +0,0 @@
-// ss_credentials.h
-// When empty (source builds / forks), the
-// firmware falls back to ss_dev_user / ss_dev_pass from config.ini.
-#pragma once
-
-#define SS_DEV_ID_EMBEDDED   ""
-#define SS_DEV_PASS_EMBEDDED ""
